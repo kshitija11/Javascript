cae734bc35bb139113f8455543baa7011fa2d502
kshitija11 <74646265+kshitija11@users.noreply.github.com>
2024-07-21 13:51:44 +0000

    variables_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12346
+let accountEmail = "abc@gmail.com"
+var accountPass = "Pass123"
+let accountHolder;
+
+// accountId = 23 ... not allowed
+accountEmail ="a@gmail.com"
+
+console.table([accountId,accountEmail,accountPass, accountHolder])
