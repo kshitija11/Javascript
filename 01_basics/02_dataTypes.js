550f1b708887fce805fdea850362efadf6c27b20
kshitija11 <74646265+kshitija11@users.noreply.github.com>
2024-07-21 14:42:12 +0000

    datatype conversion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+//conversion of data types
+let score = 33 // 33 
+// let updateScore ="33"
+
+let scoreConversion = Number(score)
+// console.table([typeof score, scoreConversion])
+// --------------------------------------------------------
+
+let isLoggedIn = 1
+let validateIsLoogedIn = Boolean(isLoggedIn)
+console.log(typeof validateIsLoogedIn, validateIsLoogedIn)
+
+//1 --> true
+// 0  ---> false
+// " " --> false
+//"hello" --> true
+
+// ----------------------------------------------------------
+let age = 33
+let updateAge = String(age)
+// console.log(typeof age, age)
+console.log(typeof updateAge, updateAge)
+
